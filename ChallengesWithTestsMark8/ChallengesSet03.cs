b1feb79e592e5c654528289037529f7ed86d5b81
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-11 16:57:08 -0600

    challenge set 4 complete.

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,62 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Contains(false);
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) return false;
+
+            return numbers.Where(x => x % 2 != 0).Sum() % 2 != 0;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            return password.Where(x => char.IsUpper(x)).Count() > 0 && password.Where(x => char.IsLower(x)).Count() > 0 && password.Where(x => char.IsNumber(x)).Count() > 0;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val.First();
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[^1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0) return 0;
+
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[^1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] to100 = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                to100[i] = i + 1;
+            }
+            return to100.Where(x => x % 2 != 0).ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+
+            // LINQ creates new array, does not modify original array
+            //words = words.Select(x => x.ToUpper()).ToArray();
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +7,58 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers.Select(x => x % 2 != 0 ? -x : x).Sum();
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            return new string[] { str1, str2, str3, str4 }.Min(x => x.Length);
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            return new int[] { number1, number2, number3, number4 }.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            int[] sides = { sideLength1, sideLength2, sideLength3 };
+
+            return sides.All(x => x > 0) && sides.OrderBy(x => x).Take(2).Sum() > sides.Max();
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            return !string.IsNullOrEmpty(input) && input.Count(x => char.IsLetter(x)) == 0 && (input.Count(x => char.IsPunctuation(x) && x != '-' && x != '.') == 0 || input.All(x => char.IsDigit(x)));
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            return objs.Count(x => x == null) > objs.Length / 2;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0) return 0;
+
+            var count = numbers.Where(x => x % 2 == 0);
+
+            return count.Any() ? count.Average() : 0;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            // base case
+            if (number == 0 || number == 1) return 1;
+            if (number < 0) throw new ArgumentOutOfRangeException();
+
+            // recursive case
+            return Factorial(number - 1) * number;
         }
     }
 }
